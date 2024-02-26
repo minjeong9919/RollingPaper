1af9bf2adcfa831b97a6ecad6c77b6f5722e32c5
Kim Min Jeong <minjeong9919@naver.com>
2024-02-26 17:50:38 +0900

    feat: 카드 컴포넌트 그리드 정렬

--- src/pages/RollingPage.jsx
+++ src/pages/RollingPage.jsx
@@ -1,19 +1,87 @@
 import React from 'react';
 import styled from 'styled-components';
 import RollingCard from '../components/rollingPage/RollingCard';
+import profileImage from '../assets/images/senderProfileImage.png';
 
 function RollingPage() {
+  const cardList = [
+    {
+      name: '김동훈',
+      badge: '동료',
+      date: '2023.07.08',
+      imgUrl: profileImage,
+      content:
+        '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!',
+    },
+    {
+      name: '강미나',
+      badge: '친구',
+      date: '2023.07.08',
+      imgUrl: profileImage,
+      content:
+        '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!',
+    },
+    {
+      name: '강미나',
+      badge: '친구',
+      date: '2023.07.08',
+      imgUrl: profileImage,
+      content:
+        '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!',
+    },
+    {
+      name: '강미나',
+      badge: '친구',
+      date: '2023.07.08',
+      imgUrl: profileImage,
+      content:
+        '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!',
+    },
+    {
+      name: '강미나',
+      badge: '친구',
+      date: '2023.07.08',
+      imgUrl: profileImage,
+      content:
+        '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!',
+    },
+    {
+      name: '강미나',
+      badge: '친구',
+      date: '2023.07.08',
+      imgUrl: profileImage,
+      content:
+        '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!',
+    },
+  ];
   return (
     <ContainerDiv>
-      <RollingCard />
+      {cardList.map((card) => {
+        return (
+          <RollingCard
+            name={card.name}
+            badge={card.badge}
+            date={card.date}
+            content={card.content}
+            imgUrl={card.imgUrl}
+          />
+        );
+      })}
     </ContainerDiv>
   );
 }
 
 const ContainerDiv = styled.div`
-  background-color: ${({ bgColor = 'var(--orange200)' }) => bgColor};
   width: 100vw;
   height: 100vw;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 384px);
+  grid-template-rows: repeat(auto-fill, 280px);
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  background-color: ${({ bgColor = 'var(--orange200)' }) => bgColor};
+  padding-top: 113px;
 `;
 
 export default RollingPage;
