2b7c4d96325ec75b4e56cffce3b0b7e401f1e193
Kim Min Jeong <minjeong9919@naver.com>
2024-02-26 14:47:43 +0900

    Design: 롤링페이지 background 설정

--- src/pages/RollingPage.jsx
+++ src/pages/RollingPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from '../styles/GlobalStyles';
 
 function RollingPage() {
-  return <div>RollingPage</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Container>RollingPage</Container>
+    </>
+  );
 }
+
+const Container = styled.div`
+  background-color: ${({ bgColor = 'var(--orange200)' }) => bgColor};
+  width: 100vw;
+  height: 100vw;
+`;
 
 export default RollingPage;
