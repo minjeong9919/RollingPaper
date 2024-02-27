758c57c9ab4c970035edb31b584925de2e360612
Kim Min Jeong <minjeong9919@naver.com>
2024-02-27 13:25:32 +0900

    fix: 모달창 구현 준비 중 오류 해결

--- src/components/rollingPage/DetailCard.jsx
+++ src/components/rollingPage/DetailCard.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { styled } from 'styled-components';
 import PropTypes from 'prop-types';
 
-function DetailCard({ visible, onClick = null }) {
-  return <BackgroundDiv visible={visible} onClick={() => onClick()} />;
+function DetailCard({ $visible, onClick = null }) {
+  return <BackgroundDiv $visible={$visible} onClick={() => onClick()} />;
 }
 
 DetailCard.propTypes = {
-  visible: PropTypes.bool.isRequired,
+  $visible: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
 const BackgroundDiv = styled.div`
   background: rgba(0, 0, 0, 0.6);
-  display: ${({ visible }) => (visible ? 'block' : 'none')};
+  visibility: ${({ $visible }) => ($visible ? 'visible' : 'hidden')};
   width: 100%;
   height: 100%;
   position: absolute;
