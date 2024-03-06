b71a11d552e1d78f2b5e7e461f5600987011563c
황준용 <wnsdyd3333@gmail.com>
2024-03-06 18:52:18 +0900

    feat: 다크모드 버튼 생성

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,23 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
 import LandingPage from '../pages/LandingPage';
 import ListPage from '../pages/ListPage';
 import RollingPage from '../pages/RollingPage';
 import RollingToBgPage from '../pages/RollingToBgPage';
 import RollingToMsgPage from '../pages/RollingToMsgPage';
+import { lightTheme, darkTheme } from '../styles/theme';
 
 function Router() {
+  const [theme, setTheme] = useState(true);
+
+  useEffect(() => {
+    const savedThemeMode = localStorage.getItem('themeMode');
+    if (savedThemeMode) {
+      setTheme(savedThemeMode === 'true');
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newThemeMode = !theme;
+    setTheme(newThemeMode);
+    localStorage.setItem('themeMode', newThemeMode);
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/post/:id" element={<RollingPage />} />
-        <Route path="/post" element={<RollingToBgPage />} />
-        <Route path="/post/:id/message" element={<RollingToMsgPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme ? lightTheme : darkTheme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage theme={theme} />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/post/:id" element={<RollingPage />} />
+          <Route path="/post" element={<RollingToBgPage />} />
+          <Route path="/post/:id/message" element={<RollingToMsgPage />} />
+        </Routes>
+        <ToggleTheme onClick={toggleTheme} />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
 export default Router;
+
+const ToggleTheme = styled.button`
+  position: fixed;
+  bottom: 50px;
+  right: 30px;
+  width: 90px;
+  height: 50px;
+  background: ${({ theme }) =>
+    theme.mode === 'light' ? '#ebebeb' : '#242424'};
+  border-radius: 200px;
+  box-shadow:
+    inset 0px 5px 15px rgba(0, 0, 0, 0.4),
+    inset 0px -5px 15px rgba(255, 255, 255, 0.4);
+  cursor: pointer;
+  transition: all 0.3s ease;
+  &::after {
+    content: '';
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    background: ${({ theme }) =>
+      theme.mode === 'light'
+        ? 'linear-gradient(180deg, #ffcc89, #d8860b)'
+        : 'linear-gradient(180deg, #777, #3a3a3a)'};
+    border-radius: 180px;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+    transform: translateX(
+      ${({ theme }) => (theme.mode === 'light' ? '0px' : '130%')}
+    );
+    transition: all 0.3s ease;
+  }
+`;
