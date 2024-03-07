9e2bdbfe52b7c7144cfa3f57278a26894fbb79aa
Kim Min Jeong <minjeong9919@naver.com>
2024-03-07 16:48:05 +0900

    design(#23): RollingToMsgPage 너비 값 수정

--- src/styles/RollingToMsgPage.style.js
+++ src/styles/RollingToMsgPage.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const MsgPageContainerDiv = styled.div`
-  max-width: 192rem;
+  /* max-width: 192rem; */
   width: 100%;
   display: flex;
   flex-direction: column;
